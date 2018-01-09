1520d6c114e077d978f431aa3e140a8c50763689
harlandd <davidharland@rentalcars.com>
2018-01-09 16:27:28 +0000

    Adding code

---
+++ src/main/java/com/dphit/gocd/GocdApplication.java
@@ -0,0 +1,12 @@
+package com.dphit.gocd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GocdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GocdApplication.class, args);
+	}
+}
---
+++ src/test/java/com/dphit/gocd/GocdApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dphit.gocd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GocdApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
